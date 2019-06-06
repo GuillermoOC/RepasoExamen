05009f99f863c31fa3f6ac9727f03f09db60ac3f
GuillermoOC <memo_shaq@hotmail.com>
2019-06-06 10:18:57 -0700

    que pedo

--- server.js
+++ server.js
@@ -1,1 +1,16 @@
 const express = require('express');
+const app = express();
+
+app.use(express.static('public'));
+app.use(express.static('styles'));
+app.set('views',__dirname+'/views');
+app.set('view engine','ejs');
+app.engine('html',require('ejs').renderFile);
+
+app.listen(3000,()=>{
+    console.log("Servidor Corriendo en el Puerto 3000");
+});
+
+app.get('/Tablero',(req, res)=>{
+    res.render('TableroPrincipal.html');
+})
